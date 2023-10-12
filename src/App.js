c15d3485dd8203b05083e9a6c37be8301391113a
Kazım Etiksan <kazim@pikselmutfak.com>
2023-10-12 10:38:00 +0300

    provider

--- src/App.js
+++ src/App.js
@@ -10,16 +10,26 @@ import Home from './Home';
 import Detail from './Detail';
 import NotFound from './NotFound';
 
+import {
+  Provider
+} from 'react-redux'
+
+import {
+  store
+} from './redux/store'
+
 const App = () => {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/view/:_id' element={<Detail />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/view/:_id' element={<Detail />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
