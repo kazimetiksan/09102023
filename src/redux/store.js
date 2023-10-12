20b6e2a78ae621be63ba7a771cb77075d4029855
Kazım Etiksan <kazim@pikselmutfak.com>
2023-10-12 10:26:51 +0300

    store

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import {
+    configureStore
+} from '@reduxjs/toolkit'
+
+import userReducer from './userSlice'
+
+export const store = configureStore({
+    reducer: {
+        users: userReducer,
+    }
+})
